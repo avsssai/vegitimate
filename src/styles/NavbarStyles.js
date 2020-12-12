13b576a521dd575ea7a1dc63a8b1aa5ac8bb174e
unknown <avsssai@gmail.com>
2020-12-12 18:14:48 +0530

    changed navbar color to white

--- src/styles/NavbarStyles.js
+++ src/styles/NavbarStyles.js
@@ -1,12 +1,13 @@
 const NavbarStyles = (theme) => ({
     navbar: {
-        background: theme.palette.lightgreen,
+        background: theme.palette.white,
         width: '100vw',
         height: '10vh',
         padding: '0 3rem',
+        border: '1px solid red',
     },
     logo: {
-        color: theme.color,
+        color: theme.palette.darkgreen,
         fontFamily: theme.fonts.pacifico,
         fontSize: theme.fonts.small,
     },
